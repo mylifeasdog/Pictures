ec5371895d2423c8f535a311ddccb74b94e0274e
Patarapon Tokham <Patarapon.t@gmail.com>
2017-07-13 18:33:53 +0700

    refactor code to clean

--- Sources/Delegate/UICollectionViewDelegate/PicturesCollectionViewDelegate.swift
+++ Sources/Delegate/UICollectionViewDelegate/PicturesCollectionViewDelegate.swift
@@ -34,33 +34,24 @@ class PicturesCollectionViewDelegate<T: UICollectionViewCell>: PicturesCollectio
         
         let result: [Any] = selectedImagePictures.isEmpty == false ? selectedImagePictures : selectedPictures
         
-        var indexedResult = [(index: UInt, picture: Any)]()
-        
-        result.forEach { element in
-            if let pictures = pictures as? [URL],
-                let picture = element as? URL
-            {
-                guard let index = pictures.index(of: picture) else
+        let indexedResult: [(index: UInt, picture: Any)] = result
+            .flatMap { element in
+                if let pictures = pictures as? [URL],
+                    let picture = element as? URL,
+                    let index = pictures.index(of: picture)
                 {
-                    return
+                    return (index: UInt(index), picture: picture)
                 }
-                
-                indexedResult.append((index: UInt(index), picture: picture))
-            }
-            else if let pictures = pictures as? [UIImage],
-                let picture = element as? UIImage
-            {
-                guard let index = pictures.index(of: picture) else
+                else if let pictures = pictures as? [UIImage],
+                    let picture = element as? UIImage,
+                    let index = pictures.index(of: picture)
                 {
-                    return
+                    return (index: UInt(index), picture: picture)
                 }
-                
-                indexedResult.append((index: UInt(index), picture: picture))
-            }
-            else
-            {
-                return
-            } }
+                else
+                {
+                    return nil
+                } }
         
         didSelectPicturesHandler?(indexedResult)
     }
